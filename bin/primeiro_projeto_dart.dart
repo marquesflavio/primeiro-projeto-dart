6a9a160e776d27a7c0c6f9a5012ad7c98fe8596d
Flávio Marques <flaviomarques2@hotmail.com>
2023-07-10 21:45:33 -0300

    criei arquivos do projeto com Dart

---
+++ bin/primeiro_projeto_dart.dart
@@ -0,0 +1,5 @@
+import 'package:primeiro_projeto_dart/primeiro_projeto_dart.dart' as primeiro_projeto_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${primeiro_projeto_dart.calculate()}!');
+}
---
+++ test/primeiro_projeto_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:primeiro_projeto_dart/primeiro_projeto_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
