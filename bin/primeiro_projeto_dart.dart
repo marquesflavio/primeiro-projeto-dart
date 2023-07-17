bae344f2d8dfd9a95c47e40827f4ce36a6e8aeb9
Flávio Marques <flaviomarques2@hotmail.com>
2023-07-17 20:23:20 -0300

    estudando tipos em Dart

--- bin/primeiro_projeto_dart.dart
+++ bin/primeiro_projeto_dart.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('Hello world!');
+  int idade = 30;
+  double tamanho = 1.74;
+  bool geek = true;
+  const String nome = 'Flávio Marques de Souza';
+  final String apelido; //o final define uma variável que "ainda" não tem valor (null), porém, uma vez definido, não muda mais.
+
+  //List<String> nomes = ['Flávio', 'Marques', 'Souza'];
+  List<dynamic> flavio = [idade, tamanho, geek, nome];
+
+  print(flavio);
+
+  String frase = 'Eu sou $nome, tenho $idade anos de idade';
+
+  print(frase);
 }
